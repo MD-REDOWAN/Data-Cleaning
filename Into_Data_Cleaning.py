1cbc87f8631b4ee1f12c5705713928e8726da024
MD-REDOWAN <hossain2305341310@diu.edu.bd>
2025-07-31 13:53:09 +0600

    Data cleaning script with dataset examples and issues

---
+++ Into_Data_Cleaning.py
@@ -0,0 +1,46 @@
+
+# Data cleaning means fixing bad data in your data set.
+
+# Bad data could be:
+
+# Empty cells
+# Data in wrong format
+# Wrong data
+# Duplicates
+
+"""
+Like there also a dataset
+
+  10        60  '2020/12/11'    103       147     329.3
+  11        60  '2020/12/12'    100       120     250.7
+  12        60  '2020/12/12'    100       120     250.7
+  13        60  '2020/12/13'    106       128     345.3
+  14        60  '2020/12/14'    104       132     379.3
+  15        60  '2020/12/15'     98       123     275.0
+  16        60  '2020/12/16'     98       120     215.2
+  17        60  '2020/12/17'    100       120     300.0
+  18        45  '2020/12/18'     90       112       NaN
+  19        60  '2020/12/19'    103       123     323.0
+  20        45  '2020/12/20'     97       125     243.0
+  21        60  '2020/12/21'    108       131     364.2
+  22        45           NaN    100       119     282.0
+  23        60  '2020/12/23'    130       101     300.0
+  24        45  '2020/12/24'    105       132     246.0
+  25        60  '2020/12/25'    102       126     334.5
+  26        60    2020/12/26    100       120     250.0
+  27        60  '2020/12/27'     92       118     241.0
+  28        60  '2020/12/28'    103       132       NaN
+  29        60  '2020/12/29'    100       132     280.0
+  30        60  '2020/12/30'    102       129     380.3
+  31        60  '2020/12/31'     92       115     243.0
+
+"""
+
+"""
+The data set contains some empty cells ("Date" in row 22, and "Calories" in row 18 and 28).
+
+The data set contains wrong format ("Date" in row 26).
+
+The data set contains wrong data ("Duration" in row 7).
+
+The data set contains duplicates (row 11 and 12)."""
